145fa4e5e220e1ee1a8fb0ac469b2379e32c78ca
Falguni307 <pfalguni307@gmail.com>
2020-09-14 18:22:44 +0530

    Comments added

--- accident in mh/accidentinmh.R
+++ accident in mh/accidentinmh.R
@@ -1,32 +1,51 @@
+# Loading Libraries
 library(XML)
 
+# Webscraping
 con=url("https://highwaypolice.maharashtra.gov.in/en/reports/")
 htmlCode = readLines(con)
 close(con)
+
+# Reading Tables into R
 recentyears <- readHTMLTable(htmlCode, which =1)
+
+# Reading Data
 head(recentyears)
 class(recentyears)
-write.csv(recentyears, "D:\\Data Science\\R\\recentyears.csv")
-recentyears <- read.csv("D:\\Data Science\\R\\recentyears.csv")
-head(recentyears)
+
+# Saving Data into csv
+write.csv(recentyears, "D:\\Data Science\\R\\accident in mh\\recentyears.csv")
+
+# Analysing Data
 summary(recentyears)
 hist(recentyears)
+
+# Checking missing values
 is.na(recentyears)
+
+# Plotting Data
 hist(recentyears$Years)
 plot(recentyears$Years, recentyears$Total.Accidents)
+
+# Reading others tables into R 
 year2016 <- readHTMLTable(htmlCode, which=2)
 year2017 <- readHTMLTable(htmlCode, which=3)
 year2018 <- readHTMLTable(htmlCode, which=4)
 year2019 <- readHTMLTable(htmlCode, which=5)
-write.csv(year2016, "D:\\Data Science\\R\\years2016.csv")
-write.csv(year2017, "D:\\Data Science\\R\\years2017.csv")
-write.csv(year2018, "D:\\Data Science\\R\\years2018.csv")
-write.csv(year2019, "D:\\Data Science\\R\\years2019.csv")
-year2016 <- read.csv("D:\\Data Science\\R\\years2016.csv")
-year2017 <- read.csv("D:\\Data Science\\R\\years2017.csv")
-year2018 <- read.csv("D:\\Data Science\\R\\years2018.csv")
-year2019 <- read.csv("D:\\Data Science\\R\\years2019.csv")
+
+# Saving the data into csv
+write.csv(year2016, "D:\\Data Science\\R\\accident in mh\\years2016.csv")
+write.csv(year2017, "D:\\Data Science\\R\\accident in mh\\years2017.csv")
+write.csv(year2018, "D:\\Data Science\\R\\accident in mh\\years2018.csv")
+write.csv(year2019, "D:\\Data Science\\R\\accident in mh\\years2019.csv")
+
+# installing & loading package
 install.packages("ggplot2")
 library(ggplot2)
+
+# Plotting the data
 qplot(Total.Accidents, year2016$total.accident, data = recentyears, geom="smooth")
 qplot(Total.Accidents, Years, data = recentyears, geom="smooth")
+
+# getting a table into R
+merged <- read.csv("D:\\Data Science\\R\\accident in mh\\merged.csv")
